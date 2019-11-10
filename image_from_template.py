d6b7609e610f1d02587035cd4152c0a8ef14bc08
Filipe Calegario <filipe.calegario@gmail.com>
2019-11-10 16:07:20 -0300

    slicing all images

--- image_from_template.py
+++ image_from_template.py
@@ -1,15 +1,25 @@
 import cv2
 
-# load and display original image
-image = cv2.imread(filename = "sources/icons-bg.jpg")
-# cv2.namedWindow(winname = "original", flags = cv2.WINDOW_NORMAL)
-# cv2.imshow(winname = "original", mat = image)
-# cv2.waitKey(delay = 0)
+columns = 7
+rows = 5
 
-# extract, display, and save sub-image
-clip = image[514:818, 0:437, :]
-# cv2.namedWindow(winname = "clip", flags = cv2.WINDOW_NORMAL)
-# cv2.imshow(winname = "clip", mat = clip)
-cv2.imwrite(filename = "clip.jpg", img = clip)
-# cv2.waitKey(delay = 0)
+image = cv2.imread(filename = "sources/icons-bg.jpg")
+
+subimg_width = 437
+subimg_height = 304
+initial_p_x = 0
+initial_p_y = 514
+
+for y in range(5):
+    for x in range(7):
+        cur_x = (initial_p_x + subimg_width * x)
+        cur_y = (initial_p_y + subimg_height * y)
+        print(cur_x,cur_y)
+        end_x = cur_x + subimg_width
+        end_y = cur_y + subimg_height
+        clip = image[cur_y:end_y, cur_x:end_x, :]
+        cv2.imwrite(filename = f'clip{x}_{y}.jpg', img = clip)      
+
+# clip = image[514:818, 0:437, :]
+
 
