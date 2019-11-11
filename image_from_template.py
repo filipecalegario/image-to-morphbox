78c64210b8f6ccdc6a590172f83d8fccb408a052
Filipe Calegario <filipe.calegario@gmail.com>
2019-11-11 13:18:14 -0300

    Adjusting the subimage slicing error

--- image_from_template.py
+++ image_from_template.py
@@ -1,25 +1,34 @@
 import cv2
+import math
 
-columns = 7
-rows = 5
+def convert_dimension_pdf_img(num_pdf):
+    return int(math.ceil((3086/841.89)*num_pdf))
 
-image = cv2.imread(filename = "sources/icons-bg.jpg")
+def convert_dimension_img_pdf(num_img):
+    return (841.89/3086)*num_img
 
-subimg_width = 437
-subimg_height = 304
-initial_p_x = 0
-initial_p_y = 514
+def slice_main_template():
+    columns = 7
+    rows = 5
 
-for y in range(5):
-    for x in range(7):
-        cur_x = (initial_p_x + subimg_width * x)
-        cur_y = (initial_p_y + subimg_height * y)
-        print(cur_x,cur_y)
-        end_x = cur_x + subimg_width
-        end_y = cur_y + subimg_height
-        clip = image[cur_y:end_y, cur_x:end_x, :]
-        cv2.imwrite(filename = f'clip{x}_{y}.jpg', img = clip)      
+    image = cv2.imread(filename = "sources/icons-bg.jpg")
 
-# clip = image[514:818, 0:437, :]
+    subimg_width = convert_dimension_pdf_img(120.27)
+    subimg_height = convert_dimension_pdf_img(84.206)
+    initial_p_x = convert_dimension_pdf_img(0)
+    initial_p_y = convert_dimension_pdf_img(139.075)
 
+    counter = 0
 
+    for y in range(5):
+        for x in range(7):  
+            cur_x = (initial_p_x + subimg_width * x)
+            cur_y = (initial_p_y + subimg_height * y)
+            print(cur_x,cur_y)
+            end_x = cur_x + subimg_width
+            end_y = cur_y + subimg_height
+            clip = image[cur_y:end_y, cur_x:end_x, :]
+            cv2.imwrite(filename = f'processed/clip{counter}_{x}_{y}.jpg', img = clip)      
+            counter = counter + 1
+
+slice_main_template()
